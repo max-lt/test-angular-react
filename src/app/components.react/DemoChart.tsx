db54e5dc3a94ddcb2e414bb2b6f74c922eb5b7b8
max-lt <maxime.tissier@gmail.com>
2025-09-13 11:59:29 +0200

    cleanup

--- src/app/components.react/DemoChart.tsx
+++ src/app/components.react/DemoChart.tsx
@@ -4,14 +4,19 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 export function DemoChart({ chartData = [] }: { chartData?: any[] }) {
   if (!chartData || chartData.length === 0) {
     return (
-      <div className="flex h-[300px] w-[500px] items-center justify-center rounded-md border border-dashed border-gray-300">
+      <div className="flex h-80 w-128 items-center justify-center rounded-md border border-dashed border-gray-300">
         <p className="text-gray-500">No data to display</p>
       </div>
     );
   }
 
   return (
-    <LineChart width={500} height={300} data={chartData}>
+    <LineChart
+      width={512}
+      height={320}
+      data={chartData}
+      className="rounded-md border-2 border-blue-600 p-4"
+    >
       <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
       <XAxis dataKey="name" />
       <YAxis />
